89070b32798842a6a8a82b5aa2a7236527449b8f
Jack Tsai <jtsai0606@gmail.com>
2018-01-26 23:57:49 -0800

    intiial click to infect and first pass of area of effect components

---
+++ Assets/AreaOfEffect.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaOfEffect : MonoBehaviour {
+
+    public float shrinkRate = 0.01F;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.localScale -= new Vector3(shrinkRate, 0, shrinkRate);
+        if (transform.localScale.x < 0)
+        {
+            Destroy(gameObject);
+        }
+	}
+
+    void OnCollisionEnter (Collision collision) {
+        
+    }
+}
---
+++ Assets/Status.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Status : MonoBehaviour {
+
+    public bool infected;
+    public GameObject infectedPrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.tag == "Person")
+                {
+                    infected = true;
+                    GameObject infectedGameObject = Instantiate(infectedPrefab, transform) as GameObject;
+                }
+            }
+        }
+	}
+}
