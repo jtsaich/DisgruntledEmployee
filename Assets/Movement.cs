e84adaf53f633cb05e36d35c0fc390370b55fd59
Jack Tsai <jtsai0606@gmail.com>
2018-01-27 13:47:04 -0800

    some adjustment on grouping path and character

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -4,33 +4,39 @@ using UnityEngine;
 using Assets.UltimateIsometricToolkit.Scripts.Core;
 
 public class Movement : MonoBehaviour {
+
+    public GameObject characterPath;
 	public float moveSpeed = 5f;
 	public int nextPoint = 0;
-	private Vector3[] path;
 
     private IsoTransform _isoTransform;
+    private CharacterPath _characterPath;
 
     void Awake() {
         _isoTransform = this.GetOrAddComponent<IsoTransform>(); //avoids polling the IsoTransform component per frame
     }
 
 	void Start() {
-		path = GetComponent<CharacterPath>().path;
-		nextPoint = Mathf.RoundToInt(Random.Range(0f, path.Length - 1));
+        nextPoint = Mathf.RoundToInt(Random.Range(0f, _characterPath.path.Length - 1));
 	}
 
 	void Update () {
+        if (_characterPath == null)
+        {
+            _characterPath = characterPath.GetComponent<CharacterPath>();
+        }
+
         int prevPoint = nextPoint - 1;
         if (prevPoint < 0)
         {
-            prevPoint = path.Length - 1;
+            prevPoint = _characterPath.path.Length - 1;
         }
 
 
-        _isoTransform.Position = Vector3.MoveTowards(_isoTransform.Position, path[nextPoint], moveSpeed * Time.deltaTime);
+        _isoTransform.Position = Vector3.MoveTowards(_isoTransform.Position, _characterPath.path[nextPoint], moveSpeed * Time.deltaTime);
 
-        if (_isoTransform.Position == path[nextPoint]) {
-			nextPoint = (nextPoint + 1) % path.Length;
+        if (_isoTransform.Position == _characterPath.path[nextPoint]) {
+            nextPoint = (nextPoint + 1) % _characterPath.path.Length;
 		}
 	}
 }
